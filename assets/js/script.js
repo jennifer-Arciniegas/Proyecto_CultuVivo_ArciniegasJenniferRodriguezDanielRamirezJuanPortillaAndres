25df4f1216ab89b3f0bfe39c12c8f4d12f123ffe
juancrfig <juancrfig@gmail.com>
2024-11-21 19:26:36 -0500

    Mejora del primer modal del index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Functions to open and close a modal
-    function openModal($el) {
-      $el.classList.add('is-active');
-    }
-  
-    function closeModal($el) {
-      $el.classList.remove('is-active');
-    }
-  
-    function closeAllModals() {
-      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-        closeModal($modal);
-      });
-    }
-  
-    // Add a click event on buttons to open a specific modal
-    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-      const modal = $trigger.dataset.target;
-      const $target = document.getElementById(modal);
-  
-      $trigger.addEventListener('click', () => {
-        openModal($target);
-      });
+  // Modal functionality
+  const openModal = ($el) => {
+    $el.classList.add('is-active');
+  };
+
+  const closeModal = ($el) => {
+    $el.classList.remove('is-active');
+  };
+
+  const closeAllModals = () => {
+    document.querySelectorAll('.modal').forEach(($modal) => {
+      closeModal($modal);
     });
-  
-    // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-      const $target = $close.closest('.modal');
-  
-      $close.addEventListener('click', () => {
-        closeModal($target);
-      });
-    });
-  
-    // Add a keyboard event to close all modals
-    document.addEventListener('keydown', (event) => {
-      if(event.key === "Escape") {
-        closeAllModals();
-      }
+  };
+
+  // Add click event on buttons to open specific modal
+  document.querySelectorAll('.js-modal-trigger').forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
     });
   });
+
+  // Add click event on various child elements to close the parent modal
+  const closeElements = document.querySelectorAll(
+    '.modal-background, .modal-close, .delete, .modal-close-button'
+  );
+
+  closeElements.forEach(($close) => {
+    const $target = $close.closest('.modal');
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    if (event.key === "Escape") {
+      closeAllModals();
+    }
+  });
+
+  // Handle form submission
+  document.getElementById('eventForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    // Add your form submission logic here
+    
+    // Close the modal after submission
+    closeAllModals();
+  });
+});
